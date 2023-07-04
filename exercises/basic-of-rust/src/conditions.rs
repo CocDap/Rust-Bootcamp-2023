924e3ca119327e66b1c765eb62f6cf06212149ae
Le Tang Phu Quy <letangphuquy4@gmail.com>
2023-07-04 08:49:33 +0700

    Finished conditions (also function's basics)

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,15 +4,16 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a > b {a} else {b}
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number == 0 { return "Zero"; }
+    if number > 0 { "Positive" } else { "Negative" }
 }
 
 // Exercise 3
@@ -22,8 +23,11 @@ fn check_number(number: u32) -> &'static str {
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
+    } else 
+    if fizzish == "fuzz" {
+        "bar"
     } else {
-        1
+        "baz"
     }
 }
 
@@ -31,14 +35,14 @@ pub fn foo_if_fizz(fizzish: &str) -> &str {
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    if n <= 0 { 1 } else { n * factorial(n - 1) }
 }
 
 // Exercise 6
@@ -46,7 +50,12 @@ fn factorial(n: u32) -> u32 {
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
+    if n <= 1 { return false; }
+    let limit: u32 = (n as f32).sqrt() as u32;
+    for i in 2..=limit {
+        if n % i == 0 { return false; }
+    }
+    true
 }
 
 
