fa8cec0938c5f7397d62994844254800559ba4ba
Alvin Ichi <48800728+nhatlapross@users.noreply.github.com>
2023-07-06 00:04:27 +0700

    Update functions.rs

--- exercises/basic-of-rust/src/functions.rs
+++ exercises/basic-of-rust/src/functions.rs
@@ -1,7 +1,7 @@
 // Exercise 1
 // Fix all errors 
-fn sum(x, y: i32) {
-    x + y;
+fn sum(x:i32 , y: i32) -> i32 {
+   return x + y;
 }
 
 //Exercise 2
@@ -11,7 +11,17 @@ fn sum(x, y: i32) {
 pub fn sum_one_to_n(n: u32) -> u32 {
     // your code for summing all digits from 1 to `n` (inclusive) should go
     // here (you can remove the sample return of `0`)
-    0
+    let mut sum:u32 = 0;
+    if n != 0{
+        for i in 1..n+1{
+            sum = sum + i;
+        }
+    } 
+    else {
+        return  0;
+    }
+    return sum;
+   
 }
 
 // Exercise 3
@@ -19,13 +29,28 @@ pub fn sum_one_to_n(n: u32) -> u32 {
 // Problem: Calculate the average of a list of numbers
 // Output: Average Number 
 fn calculate_average(numbers: &[f64]) -> f64 {
-    todo!()
+    if numbers.len() == 0 {
+        return 0.0;
+    }
+    else {
+        let mut sum:f64 = 0.0;
+        for i in numbers{
+            sum = sum + i;
+        }
+        return sum/(numbers.len() as f64);
+    }  
 }
 
 // Exercise 4
 // Calculate the sum of all even numbers in a list
 fn sum_even_numbers(numbers: &[i32]) -> i32 {
-    todo!()
+    let mut sum:i32 = 0;
+    for i in numbers{
+        if i%2 == 0 {
+            sum = sum + i;
+        }
+    }
+    return sum;
 }
 
 
@@ -37,7 +62,7 @@ mod tests {
     #[test]
     fn sum_should_work() {
         let (x, y) = (1, 2);
-        let s = sum(x, y);
+        let s:i32  = sum(x, y);
     
         assert_eq!(s, 3);
     }
