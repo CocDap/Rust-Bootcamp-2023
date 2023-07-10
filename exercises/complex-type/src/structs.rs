fef16244a107023beafb3d3d8669039e83130619
NTKhang <21520963@gm.uit.edu.vn>
2023-07-11 01:15:43 +0700

    class-3-exercises-submit1

--- exercises/complex-type/src/structs.rs
+++ exercises/complex-type/src/structs.rs
@@ -33,18 +33,18 @@ struct Agent  {
 // Implementation of methods for the Person struct
 impl Agent {
     // Create a new Person instance
-    fn new(name: String, age: u32) -> Agent {
-        Agent { name, age }
+    fn new(name: String, age: u32) -> Self {
+        Self { name, age }
     }
 
     // Get the name of the person
-    fn get_name(&self) -> &str {
-        todo!()
+    fn get_name(&self) -> String {
+        self.name.to_string()
     }
 
     // Get the age of the person
     fn get_age(&self) -> u32 {
-        todo!()
+        self.age
     }
 }
 
@@ -58,28 +58,28 @@ struct Calculator {
 
 impl Calculator {
     fn new() -> Self {
-        Calculator { value: 0 }
+        Self { value: 0 }
     }
 
-    fn add(&self, num: i32) {
+    fn add(&mut self, num: i32) {
         self.value += num;
     }
 
-    fn subtract(mut self, num: i32) {
+    fn subtract(&mut self, num: i32) {
         self.value -= num;
     }
-    fn clear(self) {
+    fn clear(&mut self) {
         self.value = 0;
     }
 
-    fn get_value(self) -> i32 {
+    fn get_value(&self) -> i32 {
         self.value
     }
 }
 
 // Exercise 4
 // Make it compile
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct User {
     first: String,
     last: String,
@@ -95,7 +95,7 @@ fn exercise4() {
 
     let u2 = User {
         first: String::from("Mary"),
-        ..u1
+        ..u1.clone()
         
     };
 
@@ -122,10 +122,10 @@ fn exercise5() {
     });
 
     
-    let moved = foos[0];
+    let moved = &foos[0];
 
     
-    let moved_field = foos[0].str_val;
+    let moved_field = &foos[0].str_val;
 }
 
 // Exercise 6
@@ -153,12 +153,12 @@ impl Package {
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        !self.sender_country.eq_ignore_ascii_case(self.recipient_country.as_str())
     }
 
-    fn get_fees(&self, cents_per_gram: i32) -> ??? {
-        // Something goes here...
+    fn get_fees(&self, cents_per_gram: i32) -> i32 {
+        self.weight_in_grams * cents_per_gram
     }
 }
 
@@ -176,7 +176,10 @@ mod tests {
             age: 30,
             hobby:String::from("Rust") 
         };
-        assert_eq!(p, p_expectation);
+        // assert_eq!(p, p_expectation);
+        assert_eq!(p.name, p_expectation.name);
+        assert_eq!(p.age, p_expectation.age);
+        assert_eq!(p.hobby, p_expectation.hobby);
         
     }
 
